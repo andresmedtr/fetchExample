947cd1f9bf6b97c842cd4a75e379b7dfcdb0a2f8
JoseR4Geeks <143768985+JoseR4Geeks@users.noreply.github.com>
2023-11-21 00:11:30 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,74 @@
-import React, { useState } from "react";
+// Home.js
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faTrashRestore } from "@fortawesome/free-solid-svg-icons";
+
 
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+  
+  const API_BASE_URL = "https://curly-system-5gq94v5pj9vv2ppgp-3000.app.github.dev";
 
+  useEffect(() => {
+   
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/tasks`);
+      const data = await response.json();
+      setTodos(data.tasks);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const addTask = async () => {
+    try {
+      await fetch(`${API_BASE_URL}/tasks`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ task: inputValue }),
+      });
+      
+      fetchTasks();
+      setInputValue("");
+    } catch (error) {
+      console.error("Error adding task:", error);
+    }
+  };
+
+  const deleteTask = async (taskId) => {
+    try {
+      await fetch(`${API_BASE_URL}/tasks/${taskId}`, {
+        method: "DELETE",
+      });
+      
+      fetchTasks();
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  };
+
+  const cleanAllTasks = async () => {
+    try {
+      // Send a DELETE request to delete all tasks on the server
+      await fetch(`${API_BASE_URL}/tasks`, {
+        method: "DELETE",
+      });
+      
+      // Update the frontend by resetting the todos state to an empty array
+      setTodos([]);
+    } catch (error) {
+      console.error("Error cleaning all tasks:", error);
+    }
+  };
+  
   return (
     <div className="container">
       <h1>todos</h1>
@@ -36,16 +99,17 @@ const Home = () => {
                   )
                 }
                 style={{ color: "#ff0000" }}
-          className="faIcon"
+                className="faIcon delete"
               />
             </li>
           ))}
         </ul>
         <div className="task">{todos.length} tasks left</div>
-        </div>
-        
-        <div className="page"></div>
+      </div>
+      
+      <div className="page"></div>
         <div className="page2"></div>
+        <button onClick={cleanAllTasks} className="CleanAll btn btn-secondary">Clean All Tasks<FontAwesomeIcon icon={faTrashRestore} className="faIcon recycle" /></button>
     </div>
   );
 };
---
+++ src/js/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+let todos = [];
+
+app.get('/tasks', (req, res) => {
+  res.json({ tasks: todos });
+});
+
+app.post('/tasks', (req, res) => {
+  const { task } = req.body;
+  todos.push({ id: todos.length + 1, text: task });
+  res.json({ success: true, message: 'Task added successfully' });
+});
+
+app.delete('/tasks/:taskId', (req, res) => {
+  const taskId = parseInt(req.params.taskId, 10);
+  todos = todos.filter((task) => task.id !== taskId);
+  res.json({ success: true, message: 'Task deleted successfully' });
+});
+
+app.delete('/tasks', (req, res) => {
+  todos = [];
+  res.json({ success: true, message: 'All tasks deleted successfully' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
